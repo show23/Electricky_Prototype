c50053d81538a4ebd1a6c679e7d51a82b085f938
ROUX <roux.hydrangea@outlook.jp>
2023-11-16 10:52:17 +0900

    メッセージウィンドウ追加

---
+++ Assets/tatsuno/MessageWindow/Messagewindow.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Messagewindow : MonoBehaviour
+{
+    public GameObject MessageWindow;
+    public TextMeshProUGUI MessageText;
+    List<string> Message = new List<string>();
+    public float WindowChangeSec = 1;
+    float ChangeSecCount;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //MessageWindow.SetActive(true);
+        //Message.Add("test");
+        //Message.Add("test2");
+        //Message.Add("test3");
+        MessageText.text = Message[0];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        UpdateMessage();
+    }
+
+    void UpdateMessage()
+    {
+        ChangeSecCount += Time.deltaTime;
+
+        if (ChangeSecCount > WindowChangeSec)
+        {
+            ChangeSecCount = 0;
+
+            if (Message.Count > 1)
+            {
+                Message.RemoveAt(0);
+                MessageText.text = Message[0];
+            }
+            else
+            {
+                MessageWindow.SetActive(false);
+                clearMessage();
+            }
+        }
+    }
+
+    public void GetList(List<string> list)
+    {
+        Message = list;
+    }
+
+    void clearMessage()
+    {
+        Message = new List<string>();
+        MessageText.text = "";
+    }
+}
---
+++ Assets/tatsuno/MessageWindow/TestMessage.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestMessage : MonoBehaviour
+{
+    public GameObject MessageWindowObj;
+    List<string> Message = new List<string>();
+    Messagewindow MessageWindow;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        MessageWindowObj.SetActive(true);
+
+        MessageWindow = MessageWindowObj.GetComponent<Messagewindow>();
+        Message.Add("test");
+        Message.Add("test2");
+        Message.Add("test3");
+        MessageWindow.GetList(Message);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
