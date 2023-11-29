cd2917ffee629eb30cb36725ebeec7653a481bd7
joy_fkd <joy.fkd@gmail.com>
2023-11-30 02:55:48 +0900

    近接の敵にアニメーション適応 ダメージをプレイヤーに反映

--- Assets/fukuda/Enemy/Script/Enemy_Blow.cs
+++ Assets/fukuda/Enemy/Script/Enemy_Blow.cs
@@ -4,50 +4,142 @@ using UnityEngine;
 
 public class Enemy_Blow : MonoBehaviour
 {
+    [System.Serializable]
     public enum EnemyState
     {
         Patrol,
-        DetectPlayer,
-        ChasePlayer
+        Chase,
+        Punch,
+        Bodyblow
     }
 
-    public Transform[] patrolPoints;
-    public float moveSpeed = 2f;
-    public float rushRadius = 5f;
-    public float rushSpeed = 5f;
-    public float chaseRadius = 10.0f;
+
+    [System.Serializable]
+    public enum BodyBlowState
+    {
+        Start,
+        Blow,
+        End
+    }
+
+    [SerializeField]
+    private Transform[] patrolPoints;
+    [SerializeField] 
+    private float moveSpeed = 2f;
+    
+    [SerializeField]
+    private float findRadius = 5f;
+    [SerializeField]
+    private float findSpeed = 5f;
+
+
+
+    [SerializeField]
+    private float punchDistance = 1.0f;
+    [SerializeField]
+    private float punchHitRadius = 1.0f;
+    [SerializeField]
+    private float punchDamage = 10.0f;
+
+
+    private int bodyBlowTimer = 0;
+
+    [SerializeField]
+    private int bodyBlowTime = 90;
+
+    [SerializeField]
+    private float bodyBlowStartDistance = 1.0f;
+    [SerializeField]
+    private float bodyBlowBiginAngle = 5.0f;
+    [SerializeField]
+    private float bodyBlowHitRadius = 1.0f;
+    [SerializeField]
+    private float bodyBlowSpeed = 1.0f;
+    [SerializeField]
+    private float bodyBlowDamage = 20.0f;
+
+    
+    private float bodyBlowLength = 0.0f;
+    private Vector3 bodyBlowStartpos;
+
+    [SerializeField]
+    private float hitKnockback = 7.5f;
+
+    //当たり判定があるかどうか
+    private bool bodyBlow_hasHit = false;
+    //すでにプレイヤーが当たったかどうか
+    private bool bodyBlow_isHit = false;
+    private bool bodyBlow_Start = false;
+    private bool bodyBlow_End = false;
+
+    private bool punch_Using = false;
+
+    [SerializeField]
+    private BodyBlowState bodyBlowState;
+
+    [SerializeField]
+    private string PlayerTag = "Player";
+
+    [SerializeField]
+    private float chaseRadius = 10.0f;
 
     private Transform player;
-    public float fieldOfViewAngle = 60.0f;
+    [SerializeField]
+    private float fieldOfViewAngle = 60.0f;
     private int currentPatrolPoint = 0;
    
     private Vector3 targetPosition;//Playrの過去の位置を保存
-    public EnemyState currentState = EnemyState.Patrol;
+    [SerializeField] EnemyState currentState = EnemyState.Patrol;
+    private Animator _animator;
+
+    [SerializeField]
+    private float MoveAnimationValue = 55;
+
+    private Vector3 oldPos;
+
+
 
     [System.Obsolete]
     private void Start()
     {
-       player = FindObjectOfType<PlayerControll>().transform;
+        _animator = GetComponentInChildren<Animator>();
+        _animator.SetBool("isAlive", true);
+        _animator.SetFloat("WalkSpeed", 0.0f);
+
+         player = FindObjectOfType<PlayerControll>().transform;
     }
-    void Update()
+    void FixedUpdate()
     {
-       
         switch (currentState)
         {
             case EnemyState.Patrol:
                 PatrolUpdate();
                 break;
 
-            case EnemyState.DetectPlayer:
-                DetectPlayerUpdate();
-                break;
-
-            case EnemyState.ChasePlayer:
+            case EnemyState.Chase:
                 ChasePlayerUpdate();
                 break;
+
+            case EnemyState.Punch:
+                PunchUpdate();
+                break;
+
+            case EnemyState.Bodyblow:
+                BodyBlowUpdate();
+                break;
         }
-        //PatrolUpdate();
+
+
+        //Debug.Log(currentState);
+
+        Vector2 vector = new Vector2(oldPos.x - transform.position.x, oldPos.z - transform.position.z);
+        _animator.SetFloat("WalkSpeed", MoveAnimationValue * vector.magnitude);
+        oldPos = transform.position;
     }
+
+
+
+
 
     void PatrolUpdate()
     {
@@ -58,17 +150,17 @@ public class Enemy_Blow : MonoBehaviour
 
         // 障害物があるか確認
         RaycastHit hit;
-        if (Physics.Raycast(transform.position, directionToPlayer, out hit, rushRadius))
+        if (Physics.Raycast(transform.position, directionToPlayer, out hit, findRadius))
         {
             if (hit.collider.CompareTag("Player"))
             {
                 // プレイヤーの方向を向く
                 Quaternion targetRotation1 = Quaternion.LookRotation(new Vector3(directionToPlayer.x, 0, directionToPlayer.z));
-                transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation1, Time.deltaTime * rushSpeed);
+                transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation1, Time.deltaTime * findSpeed);
                 transform.eulerAngles = new Vector3(0, transform.eulerAngles.y, 0);
 
-                // DetectPlayer に遷移
-                StartCoroutine(TransitionToDetectPlayer());
+                targetPosition = player.position;
+                currentState = EnemyState.Chase;
                 return;
             }
         }
@@ -85,40 +177,9 @@ public class Enemy_Blow : MonoBehaviour
     }
 
 
-
-    void DetectPlayerUpdate()
-    {
-        Debug.Log("DetectPlayer");
-
-        // プレイヤーの方向を向く
-        Vector3 directionToPlayer = (targetPosition - transform.position).normalized;
-        Quaternion targetRotation = Quaternion.LookRotation(new Vector3(directionToPlayer.x, 0, directionToPlayer.z));
-        transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, Time.deltaTime * rushSpeed);
-        transform.eulerAngles = new Vector3(0, transform.eulerAngles.y, 0);
-
-        // プレイヤーの最初の位置に向かう
-        transform.position = Vector3.MoveTowards(transform.position, targetPosition, rushSpeed * Time.deltaTime);
-
-        // 到達の閾値を設定
-        float threshold = 1.0f;
-
-        
-        if (Vector3.Distance(transform.position, targetPosition) < threshold)
-        {
-            currentState = EnemyState.ChasePlayer;
-        }
-
-        //// プレイヤーが rushRadius よりも遠い場合に Patrol 状態に遷移
-        //float distanceToPlayer = Vector3.Distance(transform.position, player.position);
-        //if (distanceToPlayer > chaseRadius)
-        //{
-        //    currentState = EnemyState.Patrol;
-        //}
-    }
-
     void ChasePlayerUpdate()
     {
-        Debug.Log("ChasePlayer");
+        bodyBlowTimer++;
 
         // プレイヤーの方向を向く
         float distanceToPlayer = Vector3.Distance(transform.position, player.position);
@@ -132,26 +193,168 @@ public class Enemy_Blow : MonoBehaviour
         transform.Translate(Vector3.forward * moveSpeed * Time.deltaTime);
 
         if (distanceToPlayer > chaseRadius)
-        {
-
             currentState = EnemyState.Patrol;
+
+        if (distanceToPlayer < punchDistance && 
+            !punch_Using)
+            currentState = EnemyState.Punch;
+
+        if (Mathf.Abs(angleToPlayer) < bodyBlowBiginAngle &&
+            bodyBlowTime < bodyBlowTimer)
+        {
+            currentState = EnemyState.Bodyblow;
+            bodyBlowTimer = 0;
         }
     }
 
-    //突進するまで
-    IEnumerator TransitionToDetectPlayer()
+    void BodyBlowUpdate()
     {
-       
-        yield return new WaitForSeconds(1.0f); // 1秒待つ
 
-        targetPosition = player.position;
-        currentState = EnemyState.DetectPlayer;
+        if (!bodyBlow_Start)
+        {
+            bodyBlowState = BodyBlowState.Start;
+            _animator.SetTrigger("RushBegin");
+            bodyBlow_isHit = false;
+            bodyBlow_End = false;
+        }
+
+        float distanceToPlayer = Vector3.Distance(transform.position, player.position);
+        Vector3 directionToPlayer = (player.position - transform.position).normalized;
+        float angleToPlayer = Vector3.Angle(transform.forward, directionToPlayer);
+        Quaternion targetRotation = Quaternion.LookRotation(new Vector3(directionToPlayer.x, 0, directionToPlayer.z));
+
+
+        switch (bodyBlowState)
+        {
+            case BodyBlowState.Start:
+                transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, Time.deltaTime * moveSpeed);
+                transform.eulerAngles = new Vector3(0, transform.eulerAngles.y, 0);
+                break;
+            case BodyBlowState.Blow:
+
+                transform.Translate(Vector3.forward * bodyBlowSpeed * Time.deltaTime);
+
+                if (bodyBlowLength < Vector3.Distance(transform.position, bodyBlowStartpos))
+                {
+                    bodyBlow_hasHit = false;
+                    bodyBlow_Start = false;
+
+                    Debug.Log("Bodyblow End (length" + bodyBlowLength + " )");
+
+                    if (punchDistance > distanceToPlayer)
+                        currentState = EnemyState.Punch;
+                    else
+                        _animator.SetTrigger("RushEnd");
+                }
+
+                //当たり判定
+
+                if (!bodyBlow_isHit)
+                {
+                    if (bodyBlow_hasHit)
+                      bodyBlow_isHit = AttackHitCheck(bodyBlowHitRadius, bodyBlowDamage, hitKnockback);
+                }
+                break;
+            case BodyBlowState.End:
+                if (bodyBlow_End)
+                {
+                    bodyBlowState = BodyBlowState.Start;
+                    currentState = EnemyState.Chase;
+                    bodyBlowTimer = 0;
+                }
+                break;
+            default :
+                currentState = EnemyState.Chase;
+                break;
+         }
+
+    }
+
+    public void RushStart()
+    {
+        bodyBlow_Start = true;
+        Debug.Log("Bodyblow Start (Timer is " + bodyBlowTimer + " )");
+    }
+
+    public void RushHitStart()
+    {
+        bodyBlow_hasHit = true; 
+        bodyBlowStartpos = transform.position;
+        bodyBlowLength = Vector3.Distance(transform.position ,player.position) + 4.0f;
+        bodyBlowState = BodyBlowState.Blow;
     }
     
+    
+    public void RushHitEnd()
+    {
+        bodyBlow_hasHit = false;
+    }
+    
+    public void RushEnd()
+    {
+        bodyBlow_End = true;
+        bodyBlowState = BodyBlowState.End;
+    }
+
+
+
+    void PunchUpdate()
+    {
+        if (!punch_Using)
+        {
+            _animator.SetTrigger("Punch");
+        }
+    }
+
+
+    public void PunchStart()
+    {
+        punch_Using = true;
+    }
+    public void PunchHit()
+    {
+        //当たり判定-------------
+        AttackHitCheck(punchHitRadius, punchDamage, hitKnockback);
+    }
+
+    public void PunchEnd()
+    {
+        currentState = EnemyState.Chase;
+        punch_Using = false;
+    }
+
+
+    bool AttackHitCheck(float range, float damage, float knockback)
+    {
+        bool isHit = false;
+        Collider[] hitColliders = Physics.OverlapSphere(transform.position, range);
+        foreach (Collider col in hitColliders)
+        {
+            if (col.CompareTag(PlayerTag))
+            {
+                Vector3 direction = Vector3.Normalize( Vector3.Scale(col.transform.position - transform.position, new Vector3(1,0,1)));
+
+                //ここで敵の体力を減らす処理をする
+                //ダメージ量はdamageから使ってください
+                GameObject player = col.gameObject;
+                player.GetComponent<PlayerControll>().CurrentHp -= damage;
+                player.GetComponent<Rigidbody>().AddForce(direction * knockback, ForceMode.Impulse);
+
+                Debug.Log("Player Damaged (damage " + damage + " )");
+                isHit = true;
+                break; //プレイヤーは1人しかいないのでbreak
+            }
+        }
+
+        return isHit;
+    }
+
+
+
     private void OnDrawGizmos()
     {
         Gizmos.color = Color.black;
-        Gizmos.DrawWireSphere(transform.position, rushRadius);
+        Gizmos.DrawWireSphere(transform.position, findRadius);
         Gizmos.color = Color.blue;
         Gizmos.DrawWireSphere(transform.position, chaseRadius);
     }
