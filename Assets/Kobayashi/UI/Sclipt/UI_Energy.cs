bb1061c072af1d6d1b8c512444059ec00027acc0
MSI\kjiku <kokudenashibeat@gmail.com>
2023-12-02 20:00:51 +0900

    n

---
+++ Assets/Kobayashi/UI/Sclipt/UI_Energy.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UI_Energy : MonoBehaviour
+{
+    [SerializeField] protected Image _gaugeImage;
+
+    private PlayerControll _playerControl;
+
+    private float _lastEnergy;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _playerControl = GameObject.Find("Player").GetComponent<PlayerControll>();
+
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        /*電気ゲージは頻繁に変化するし常に更新する方針で良いか
+          更新時に前フレームと変化あるか見る
+        */
+
+        
+    }
+}
