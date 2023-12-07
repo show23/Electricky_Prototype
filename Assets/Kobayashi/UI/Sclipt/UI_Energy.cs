3726d7605f43882c50e9a28906f99d8083772826
MSI\kjiku <kokudenashibeat@gmail.com>
2023-12-07 14:28:24 +0900

    tsurai

--- Assets/Kobayashi/UI/Sclipt/UI_Energy.cs
+++ Assets/Kobayashi/UI/Sclipt/UI_Energy.cs
@@ -2,10 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class UI_Energy : MonoBehaviour
 {
     [SerializeField] protected Image _gaugeImage;
+    [SerializeField] protected TextMeshProUGUI _text;
 
     private PlayerControll _playerControl;
 
@@ -26,6 +28,17 @@ public class UI_Energy : MonoBehaviour
           更新時に前フレームと変化あるか見る
         */
 
-        
+        float energy_now = _playerControl.CurrentEnergy;
+        float energy_max = _playerControl.CurrentMaxEnergy;
+
+        if(_lastEnergy ==  energy_now) 
+        {
+            _gaugeImage.fillAmount = energy_now / energy_max;
+
+
+            _text.text = energy_now.ToString("F0");
+        }
+
+        _lastEnergy  = energy_now;
     }
 }
