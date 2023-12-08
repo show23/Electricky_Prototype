fa3773d21f7a6671028efa2b304d220139e63638
MSI\kjiku <kokudenashibeat@gmail.com>
2023-12-08 17:35:45 +0900

    lifewakaran

--- Assets/Kobayashi/UI/Sclipt/UI_Life.cs
+++ Assets/Kobayashi/UI/Sclipt/UI_Life.cs
@@ -4,10 +4,12 @@ using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
 
-public class UI_Energy : MonoBehaviour
+public class UI_Life : MonoBehaviour
 {
+    [SerializeField] protected Slider _slider;
     [SerializeField] protected Image _gaugeImage;
     [SerializeField] protected TextMeshProUGUI _text;
+    [SerializeField] protected FadeImage _fadeImage;
 
     private PlayerControll _playerControl;
 
@@ -17,24 +19,14 @@ public class UI_Energy : MonoBehaviour
     void Start()
     {
         _playerControl = GameObject.Find("Player").GetComponent<PlayerControll>();
-
-        
     }
 
     // Update is called once per frame
     void Update()
     {
-        float energy_now = _playerControl.CurrentEnergy;
-        float energy_max = _playerControl.CurrentMaxEnergy;
+        //float energy_now = _playerControl.CurrentEnergy;
+        //float energy_max = _playerControl.CurrentMaxEnergy;
 
-        if(_lastEnergy ==  energy_now) 
-        {
-            _gaugeImage.fillAmount = energy_now / energy_max;
-
-
-            _text.text = energy_now.ToString("F0");
-        }
-
-        _lastEnergy  = energy_now;
+        //_fadeImage.Range = 1.0f - (energy_now / energy_max);
     }
 }
