bd8e4da408d24c688eb11a45d92c5122e0c11fad
show23 <oshimashota2373@gmail.com>
2023-12-18 01:02:06 +0900

    Light途中

---
+++ Assets/Oshima/Scripts/LightFrash.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightFrash : MonoBehaviour
+{
+    public GameObject startLight;
+    Light lightInstance;
+    // Start is called before the first frame update
+    void Start()
+    {
+        lightInstance = startLight.GetComponent<Light>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        StartCoroutine(Frash(0f, 1f));
+    }
+
+    private void LightIntensity(float intenceAmount)
+    {
+        lightInstance.intensity = intenceAmount;
+    }
+
+    IEnumerator Frash(float intenceAmount, float intenceAmount2)
+    {
+        LightIntensity(intenceAmount);
+        yield return new WaitForSeconds(0.8f);
+        LightIntensity(intenceAmount2);
+    }
+}
---
+++ Assets/Oshima/Scripts/MaterialChange.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MaterialChange : MonoBehaviour
+{
+    //点滅させる光を取得（インスペクター側でアタッチしてください）
+    public GameObject startLight;
+    //光の強さ取得するための変数を作る
+    Light lightIntence;
+
+    void Start()
+    {
+        //光の強さ(Intensity)を取得。
+        lightIntence = startLight.GetComponent<Light>();
+    }
+    void Update()
+    {
+        //後述するフラッシュ関数をコルーチンで開始。
+        //光の強さを0と1に0.7秒間隔で表示させる。
+        StartCoroutine(Frash(0f, 1f));
+    }
+
+
+    //光の強さを決定できる関数（引数で調整できる）。
+    private void LightIntencity(float intenceAmount)
+    {
+        lightIntence.intensity = intenceAmount;
+    }
+
+
+    //光の点滅関数。
+    IEnumerator Frash(float intenceAmount, float intenceAmount2)
+    {
+        LightIntencity(intenceAmount);
+        yield return new WaitForSeconds(0.7f);
+        LightIntencity(intenceAmount2);
+    }
+}
