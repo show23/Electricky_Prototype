43f26b40698f709f69b43cacbaa7dc8c1a63ab74
show73 <show23732373@gmail.com>
2023-12-18 11:34:01 +0900

    no message

---
+++ Assets/Oshima/Scripts/RendererOnOffExample.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RendererOnOffExample : MonoBehaviour
+{
+    // 点滅させる対象
+    [SerializeField] private Renderer _target;
+    // 点滅周期[s]
+    [SerializeField] private float _cycle = 1;
+
+    private double _time;
+
+    private void Update()
+    {
+        // 内部時刻を経過させる
+        _time += Time.deltaTime;
+
+        // 周期cycleで繰り返す値の取得
+        // 0～cycleの範囲の値が得られる
+        var repeatValue = Mathf.Repeat((float)_time, _cycle);
+
+        // 内部時刻timeにおける明滅状態を反映
+        _target.enabled = repeatValue >= _cycle * 0.5f;
+    }
+}
