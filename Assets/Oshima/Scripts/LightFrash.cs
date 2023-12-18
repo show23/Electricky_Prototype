471a724f74e28b5543ea7fed24c09a2b57091896
show23 <oshimashota2373@gmail.com>
2023-12-18 20:09:14 +0900

    Light経過

--- Assets/Oshima/Scripts/LightFrash.cs
+++ Assets/Oshima/Scripts/LightFrash.cs
@@ -4,29 +4,23 @@ using UnityEngine;
 
 public class LightFrash : MonoBehaviour
 {
-    public GameObject startLight;
-    Light lightInstance;
-    // Start is called before the first frame update
-    void Start()
-    {
-        lightInstance = startLight.GetComponent<Light>();
-    }
+    // 点滅させる対象
+    [SerializeField] private Renderer _target;
+    // 点滅周期[s]
+    [SerializeField] private float _cycle = 1;
 
-    // Update is called once per frame
-    void Update()
-    {
-        StartCoroutine(Frash(0f, 1f));
-    }
+    private double _time;
 
-    private void LightIntensity(float intenceAmount)
+    private void Update()
     {
-        lightInstance.intensity = intenceAmount;
-    }
+        // 内部時刻を経過させる
+        _time += Time.deltaTime;
 
-    IEnumerator Frash(float intenceAmount, float intenceAmount2)
-    {
-        LightIntensity(intenceAmount);
-        yield return new WaitForSeconds(0.8f);
-        LightIntensity(intenceAmount2);
+        // 周期cycleで繰り返す値の取得
+        // 0～cycleの範囲の値が得られる
+        var repeatValue = Mathf.Repeat((float)_time, _cycle);
+
+        // 内部時刻timeにおける明滅状態を反映
+        _target.enabled = repeatValue >= _cycle * 0.5f;
     }
 }
