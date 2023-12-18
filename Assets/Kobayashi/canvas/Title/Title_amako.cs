1f9db334e7084f4b8fd9114b1dfb6148298cb24f
AMASAN\hiros <hiroshiamakoson@gmail.com>
2023-12-18 21:19:14 +0900

    Title画面

---
+++ Assets/Kobayashi/canvas/Title/Title_amako.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+using UnityEngine.UI;
+
+public class Title_amako : MonoBehaviour
+{
+    [SerializeField] VideoPlayer video;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(video.time >= 0.5f)
+        {
+            this.gameObject.SetActive(false);
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        
+    }
+}
