2d8f6b3a340d089eb16bca6495222678752da090
ROUX <roux.hydrangea@outlook.jp>
2023-12-19 23:15:28 +0900

    タイトルからゲームシーンへ遷移

---
+++ Assets/Kobayashi/canvas/Title/Title_nextscene.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Title_nextscene : MonoBehaviour
+{
+    bool nextscenecheck = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void NextScene()
+    {
+        if(!nextscenecheck)
+        {
+            SceneManager.LoadScene("Main_Scene");
+            nextscenecheck = true;
+        }
+    }
+}
