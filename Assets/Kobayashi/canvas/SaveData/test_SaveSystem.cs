29a9d361c57810242c76e01d7c09e06d42df199f
MSI\kjiku <kokudenashibeat@gmail.com>
2023-12-19 19:36:25 +0900

    Non Active

--- Assets/Kobayashi/canvas/SaveData/test_SaveSystem.cs
+++ Assets/Kobayashi/canvas/SaveData/test_SaveSystem.cs
@@ -7,8 +7,9 @@ public class test_SaveSystem : MonoBehaviour
 {
     [SerializeField] private SaveSystem _saveSystem;
     [SerializeField] private TextMeshProUGUI _textNow;
-    [SerializeField] private TextMeshProUGUI _textRank;
 
+    [SerializeField] private TextMeshProUGUI[] _rankTime;
+    [SerializeField] private TextMeshProUGUI[] _numName;
 
     //private rankData testDataTime;
 
@@ -17,9 +18,24 @@ public class test_SaveSystem : MonoBehaviour
     void Start()
     {
 
-        _saveSystem.Save(1, 49f, 0);
+        //_saveSystem.Save(1, 49f, 0);
 
-        Debug.Log(_saveSystem.BestData.Length);
+        _saveSystem.Load();
+
+        int minute = _saveSystem.ThisData.minute;
+        float seconds = _saveSystem.ThisData.seconds;
+        float timedecimal = seconds * 100 - (int)seconds * 100;
+
+        _textNow.text = minute.ToString("00") + ":" + ((int)seconds).ToString("00") + ":" + ((int)timedecimal).ToString("00");
+
+        for (int i = 0; i < _rankTime.Length; i++)
+        {
+            minute = _saveSystem.BestData[i].minute;
+            seconds = _saveSystem.BestData[i].seconds;
+            timedecimal = seconds * 100 - (int)seconds * 100;
+
+            _rankTime[i].text = minute.ToString("00") + ":" + ((int)seconds).ToString("00") + ":" + ((int)timedecimal).ToString("00");
+        }
     }
 
     // Update is called once per frame
