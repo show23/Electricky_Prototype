6e1d03bd80f8289587ca48e27a04e76e0533a005
MSI\kjiku <kokudenashibeat@gmail.com>
2023-12-23 21:35:02 +0900

    Add SE

---
+++ Assets/Kobayashi/Audio/Script/PlaySE.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaySE : MonoBehaviour
+{
+    [SerializeField] private AudioClip sounces01;
+
+    private AudioSource audioSource;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+        audioSource.PlayOneShot(sounces01);
+    }
+
+}
