1a2fa02a70f7c46cb4711dee3b57c736ceb40eed
joy_fkd <joy.fkd@gmail.com>
2024-01-19 14:44:44 +0900

    ゲームの強制リセット機能追加(Shift+Enter)

---
+++ Assets/fukuda/GameResetScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameResetScript : MonoBehaviour
+{
+    [Header("LeftShift + Enter でリセット")]
+
+    [Header("タイトルシーン名をここに設定してください")]
+    
+    [SerializeField]
+    private string StartSceneName;
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (Input.GetKey("LeftShift"))
+        {
+            if (Input.GetKeyDown("Enter"))
+            {
+                SceneManager.LoadScene(StartSceneName);
+            }
+        }
+    }
+}
